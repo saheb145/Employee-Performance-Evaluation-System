f402798b4baa38d349f0d24dd6b3fb91834823bf
ankitakaakj <138436569+Philosopherakj@users.noreply.github.com>
2023-09-15 19:00:47 +0530

    added api for self evauation

---
+++ EPES.Services.PerformanceEvaluationAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EPES.Services.PerformanceEvaluationAPI.Models.Dto;
+using EPES.Services.PerformanceEvaluationAPI.Models;
+
+namespace EPES.Services.PerformanceEvaluationAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<SelfEvaluationDto, SelfEvaluation>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
