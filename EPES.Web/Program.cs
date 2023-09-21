ca925c848af1644f11bb5e048618737a2b89186c
Saheb kumar sah <sahebkumarsah1500250@gmail.com>
2023-09-21 08:18:17 +0530

    saheb,s commit

--- EPES.Web/Program.cs
+++ EPES.Web/Program.cs
@@ -14,14 +14,18 @@ builder.Services.AddHttpClient();
 
 builder.Services.AddHttpClient<IEmployeeService, EmployeeService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
+builder.Services.AddHttpClient<UserService, UserService>();
 
 SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
+SD.UserAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 SD.UserMangementAPIBase = builder.Configuration["ServiceUrls:UserMangementAPI"];
 
 builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
