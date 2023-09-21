bfecb9cf6ebc58333c7b8c9839ee6dc3a059eee0
Saheb kumar sah <sahebkumarsah1500250@gmail.com>
2023-09-21 07:31:12 +0530

    saheb

---
+++ EPES.Services.AuthAPI/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using EPES.Services.AuthAPI.Data;
+using EPES.Services.AuthAPI.Models;
+using EPES.Services.AuthAPI.Models.Dto;
+using EPES.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPES.Services.AuthAPI.Controllers
+{
+	/* [Route("api/[Controller]")]
+	 //[Route("api/user")]
+	 [ApiController]*/
+	/*public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService=userService;
+        }
+		[HttpGet]
+		public async Task<object> Get()
+		{
+			IEnumerable<ApplicationUser> response = await _userService.GetAllUsers();
+            return response;
+			
+		}
+		
+    }*/
+
+	[Route("api/user")]
+	[ApiController]
+//	[Authorize(Roles = "MANAGER")]
+	public class UserController : ControllerBase
+	{
+		private readonly AppDbContext _db;
+		private ResponseDto _response;
+		private IMapper _mapper;
+		private readonly UserManager<ApplicationUser> _userManager;
+		public UserController(AppDbContext db,IMapper mapper, UserManager<ApplicationUser> userManager)
+		{
+			_db = db;
+			_response = new ResponseDto();
+			_mapper = mapper;
+			_userManager = userManager;
+		}
+		[HttpGet]
+		public ResponseDto Get()
+		{
+			try
+			{
+				var employees = _userManager.GetUsersInRoleAsync("employee").Result;
+
+				// Map the list of employees to ApplicationUserDto
+				var employeeDtos = _mapper.Map<IEnumerable<ApplicationUserDto>>(employees);
+				_response.Result = employeeDtos;
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false; 
+				_response.Message = ex.Message;
+			}
+
+			return _response;
+
+		}
+
+	}
+}
