17f0c7d82ed0706069946088559bb07d48672b96
Saheb kumar sah <sahebkumarsah1500250@gmail.com>
2023-09-22 21:28:40 +0530

    comitted

--- EPES.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ EPES.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -74,8 +74,21 @@ namespace EPES.Services.AuthAPI.Controllers
             }
 
         }
-        
-       
+
+       /* [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+        {
+            var loginResponse = await _authService.Login(model);
+            if (loginResponse.User == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Username or password is incorrect";
+                return BadRequest(_response);
+            }
+            _response.Result = loginResponse;
+            return Ok(_response);
+
+        }*/
 
         [HttpPost("AssignRole")]
         public async Task<IActionResult> AssignRole([FromBody] RegistrationRequestDto model)
